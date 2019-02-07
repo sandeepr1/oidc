2e1be6083c2bca15a99660ac34e4012448aa047f
sandeepr1 <venkatasandee@gmail.com>
2019-02-07 23:11:46 +0000

    modfied url

--- app.js
+++ app.js
@@ -17,11 +17,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.get("/authorize", (req, res, next) => {
-    console.log(req);
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.set({'Location':'https://google.com'});
-    res.status(400).send({status:true});
+    res.set({'Location':'http://127.0.0.1:8081/index.html'});
+    res.status(302).send({status:true});
    });
 
 module.exports = app;
