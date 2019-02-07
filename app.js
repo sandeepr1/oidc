4813874465e09c0804924a629da256aef48f50de
sandeepr1 <9305800@lloydsbanking.com>
2019-02-07 22:18:36 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var cors = require('cors')
+var app = express();
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.get("/authorize", (req, res, next) => {
+    console.log(req);
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.set({'Location':'https://google.com'});
+    res.status(400).send({status:true});
+   });
+
+module.exports = app;
